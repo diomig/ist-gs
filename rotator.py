1093dab07c5e33f541a61cd35b7a477780b74d74
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-06-20 15:56:22 +0100

    close the rotator at any exception

--- rotator.py
+++ rotator.py
@@ -96,3 +96,6 @@ if __name__ == "__main__":
     except (KeyboardInterrupt, subprocess.TimeoutExpired):
         rot.end()
         print("\nRotator was interrupted")
+    except Exception:
+        rot.end()
+        print('\nSome problem occured')
