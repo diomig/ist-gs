e66e555e1627b90af3835482d1347c4f6a3d6579
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-06-20 14:35:42 +0100

    linked rotator configuration file to Rotator class

--- rotator.py
+++ rotator.py
@@ -1,17 +1,19 @@
 import socket
 import subprocess
 import time
+from lib.configuration import rot_configutarion as conf
 
-host = "localhost"
-port = 4533
-model = "1"
+# host = "localhost"
+# port = 4533
+# model = "1"
 
 
 class Rotator:
-    def __init__(self, host, port, model):
+    def __init__(self, host, port, model, verbose=False):
         self.host = host
         self.port = port
         self.model = model
+        self.verbose = verbose
         self.start_daemon()
         time.sleep(1)
         self.open_socket()
@@ -26,7 +28,7 @@ class Rotator:
                 self.host,
                 "-t",
                 str(self.port),
-                "-vvvvvvvv",
+                "-vvvv" if self.verbose else '',
             ]
         )
 
@@ -41,29 +43,27 @@ class Rotator:
     def close_socket(self):
         self.socket.close()
 
-    def get_pos(self):
-        cmd = "p\x0a"
+    def command(self, cmd: str):
         buffsize = 20
-        self.socket.send(cmd.encode())
-        return self.socket.recv(buffsize).decode().split()
+        self.socket.send((cmd + "\x0a").encode())
+        return self.socket.recv(buffsize).decode()
+
+    def get_pos(self):
+        return self.command('p').split()
 
     def set_pos(self, az, el):
         cmd = f"P {az} {el}\x0a"
-        buffsize = 20
-        self.socket.send(cmd.encode())
-        return self.socket.recv(buffsize).decode()
+        return self.command(cmd)
 
     def end(self):
-        cmd = 'q\x0a'
-        self.socket.send(cmd.encode())
-        print(self.socket.recv(20))
+        self.command('q')
         self.close_socket()
         self.terminate_daemon()
         print("\nRotator Terminated")
 
 
 if __name__ == "__main__":
-    rot = Rotator(host, port, model)
+    rot = Rotator(conf.host, conf.port, conf.model, verbose=True)
 
     try:
         while True:
