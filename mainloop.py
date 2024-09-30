8d7e9c81eeec2f4565d218c08d831bf16abeef19
diomig <diogo.m.luis@tecnico.ulisboa.pt>
2024-09-30 16:52:31 +0100

    start integration of main loop commands

--- mainloop.py
+++ mainloop.py
@@ -2,7 +2,7 @@ import gs_setup as setup
 import shell_utils as su
 from MQTT import globalName, mqttC, mqttTopics, pub_topics
 from rotator import rot
-
+import gs_shell
 
 def on_message(client, userdata, msg):
     topic = msg.topic
@@ -33,7 +33,9 @@ mqttC.loop_start()
 
 if __name__ == "__main__":
     try:
-        radio_setup()
+        radio = radio_setup()
+        gs_shell.print_help()
+        gs_shell.gs_shell_main_loop(radio)
         while True:
             cmd = input("user cmd:").split()
             if len(cmd) > 1:
